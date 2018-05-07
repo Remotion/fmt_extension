341a05d6e38f9198a396b348bd8f46a3931b3d9e
Remotion <remotion4d@gmail.com>
2018-05-07 23:18:28 +0200

    Added cmake 3.11 and premake 5 support.

---
+++ cmake_vc15_build.bat
@@ -0,0 +1,43 @@
+@echo off
+
+CD /D %~dp0
+
+echo #############################################################################################
+echo Starting %~n0 %date% %time%
+echo #############################################################################################
+
+set PROJECT_NAME=fmt_extension
+set EXE_NAME=fmt_extension
+
+md build_vs2017
+cd build_vs2017
+cmake .. -G"Visual Studio 15 2017 Win64" -DCMAKE_SUPPRESS_REGENERATION=true 
+if %ERRORLEVEL% NEQ 0 (
+  echo "Error: " %ERRORLEVEL%
+  pause
+  exit 
+)
+
+@call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat"
+
+msbuild "%PROJECT_NAME%.sln" /p:Configuration=debug /p:Platform=x64 /m 
+if %ERRORLEVEL% NEQ 0 (
+  echo "Error: " %ERRORLEVEL%
+  pause
+  exit
+)
+
+echo #############################################################################################
+echo Starting "%EXE_NAME%.exe" %date% %time%
+echo #############################################################################################
+echo.
+call "debug/%EXE_NAME%.exe"
+echo.
+echo _____________________________________________________________________________________________
+pause 
+
+:: --open ???
+:: --open testProj.sln
+:: -DCMAKE_USE_RELATIVE_PATHS=true 
+:: -DCMAKE_COLOR_MAKEFILE=ON
+::pause 
